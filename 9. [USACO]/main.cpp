0745b74079124c1d282b400e086a3746665eef30
Zhunussova <zhazira173@gmail.com>
2014-09-27 19:19:08 +0600

    project initiation

---
+++ 9. [USACO]/main.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+#include <utility>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <cstdio>   
+#include <cmath>
+#include <queue>
+#include <ctime>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int>  pi;
+typedef vector<int>    vi;
+typedef vector<pi>	   vpi;
+typedef vector<vi>	   vvi;
+typedef queue<pi> qpi;
+
+const int mod = int(1e9 + 7);
+const double PI = acos(-1.0);
+
+#define ones(x) (__builtin_popcount(x))
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define sqr(x) ((x)*(x))
+
+#define len length()
+#define sz size()
+#define pb push_back
+#define mp make_pair
+#define s second
+#define f first
+
+#define inf 1000000000
+#define linf 100000000000ll
+#define eps (1e-7)
+
+#define forit(it,S) for(__typeof(S.begin()) it = S.begin(); it != S.end(); ++it)
+#define Fill(a,x) memset(a, x, sizeof(a))
+
+#define For(i,a,b)  for(int _b=(b), i=(a); i <= _b; i++)
+#define ForD(i,a,b) for(int i=(a), _b=(b); i >= _b; i--)
+
+#define all(c) (c).begin(), (c).end()
+#define Sort(c) sort(all(c))
+#define Rev(c) reverse(all(c))
+
+int main (){
+	#ifndef local
+	freopen ("input.txt", "r", stdin);
+	freopen ("output.txt", "w", stdout);
+	#endif	
+
+	return 0;
+}
